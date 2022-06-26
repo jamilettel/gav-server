62aaac103fa77d0e8894c8e8947f0290f321779d
Jamil ETTEL <jamil.ettel@epitech.eu>
2022-06-26 23:46:32 +0100

    🚧 Working on ga_server

---
+++ ga_server/ga_simple.py
@@ -0,0 +1,28 @@
+from deap import base
+from deap import tools
+from deap import algorithms
+
+def ga_simple(
+        pop,
+        toolbox: base.Toolbox,
+        cxpb: float,
+        mutpb: float,
+        ngen: int,
+        stats: tools.Statistics = None,
+        hof: tools.HallOfFame = None):
+    for _ in range(ngen):
+        offspring = toolbox.select(pop, len(pop))
+        offspring = algorithms.varAnd(offspring, toolbox, cxpb, mutpb)
+        fitness = toolbox.map(toolbox.evaluate, offspring)
+        for fit, ind in zip(fitness, offspring):
+            ind.fitness.values = fit
+
+        if hof is not None:
+            hof.update(offspring)
+
+        pop[:] = offspring
+        if stats is not None:
+            record = stats.compile(pop)
+            print(record)
+
+
---
+++ tsp.py
@@ -0,0 +1,80 @@
+#    This file is part of DEAP.
+#
+#    DEAP is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as
+#    published by the Free Software Foundation, either version 3 of
+#    the License, or (at your option) any later version.
+#
+#    DEAP is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#    GNU Lesser General Public License for more details.
+#
+#    You should have received a copy of the GNU Lesser General Public
+#    License along with DEAP. If not, see <http://www.gnu.org/licenses/>.
+
+import array
+from os import getpid
+import random
+import json
+
+import numpy
+
+from ga.ga_simple import ga_simple
+
+from deap import algorithms
+from deap import base
+from deap import creator
+from deap import tools
+
+# gr*.json contains the distance map in list of list style in JSON format
+# Optimal solutions are : gr17 = 2085, gr24 = 1272, gr120 = 6942
+with open("tmp.json", "r") as tsp_data:
+    tsp = json.load(tsp_data)
+
+distance_map = tsp["DistanceMatrix"]
+IND_SIZE = tsp["TourSize"]
+
+creator.create("FitnessMin", base.Fitness, weights=(-1.0,))
+creator.create("Individual", array.array, typecode='i', fitness=creator.FitnessMin)
+
+toolbox = base.Toolbox()
+
+# Attribute generator
+toolbox.register("indices", random.sample, range(IND_SIZE), IND_SIZE)
+
+# Structure initializers
+toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.indices)
+toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+def evalTSP(individual):
+    distance = distance_map[individual[-1]][individual[0]]
+    for gene1, gene2 in zip(individual[0:-1], individual[1:]):
+        distance += distance_map[gene1][gene2]
+    return distance,
+
+toolbox.register("mate", tools.cxPartialyMatched)
+toolbox.register("mutate", tools.mutShuffleIndexes, indpb=0.05)
+toolbox.register("select", tools.selTournament, tournsize=3)
+toolbox.register("evaluate", evalTSP)
+
+def main():
+    random.seed(getpid())
+
+    pop = toolbox.population(n=300)
+
+    hof = tools.HallOfFame(1)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("std", numpy.std)
+    stats.register("max,min,mean", lambda x: [numpy.max(x), numpy.min(x), numpy.mean(x)])
+
+    ga_simple(pop, toolbox, 0.7, 0.2, 1, stats=stats, hof=hof)
+    # algorithms.eaSimple(pop, toolbox, 0.7, 0.2, 40, stats=stats,
+    #                     halloffame=hof)
+
+    print(f'Best distance found: {hof.items[0].fitness.values[0]}')
+    print(f'Optimal distance {tsp["OptDistance"]}')
+    return pop, stats, hof
+
+if __name__ == "__main__":
+    main()
